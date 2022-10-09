3ed5beb2fff25298cdfc3efe0635ccb00e5757da
Badhon Nath Joy <8ad40n@gmail.com>
2022-10-09 12:07:46 +0600

    .

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,26 @@
+//A;
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl        "\n"
+#define ll          long long
+#define pb          push_back
+#define pop         pop_back
+#define FOR         for(int i=0;i<n;i++)
+#define yes         cout<<"YES"<<endl;
+#define no          cout<<"NO"<<endl;
+#define BOOST       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi          3.141592653589793238
+int main(){
+BOOST;
+int a,b;
+cin>>a>>b;
+int sum;
+
+if(a<200 && b<200){
+    sum=a+b;
+}
+cout<<sum;
+}
+
