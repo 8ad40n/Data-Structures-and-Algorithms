fc0b03b3bb0858067677e21623e62543507d5913
Badhon Nath Joy <8ad40n@gmail.com>
2022-11-26 00:52:01 +0600

    added

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,45 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int knapSack(int W, int wt[], int val[], int n)
-{
-	// making and initializing dp array
-	int dp[W + 1];
-	memset(dp, 0, sizeof(dp));
-
-
-// Memset() is a C++ function. It copies a single character 
-// for a specified number of times to an object. It is useful 
-// for filling a number of bytes with a given value starting 
-// from a specific memory location. It is defined in <cstring> header file.
-
-// int main()
-// {
-//     char str[] = "geeksforgeeks";
-//     memset(str, 't', sizeof(str));
-//     cout << str;
-//     return 0;
-// }
-// output: tttttttttttttt
-
-
-
-
-	for (int i = 1; i < n + 1; i++) {
-		for (int w = W; w >= 0; w--) {
-
-			if (wt[i - 1] <= w)
-				// finding the maximum value
-				dp[w] = max(dp[w],dp[w - wt[i - 1]] + val[i - 1]);
-		}
-	}
-	return dp[W]; // returning the maximum value of knapsack
-}
 int main()
 {
-	int val[] = { 3,4,5,6 };
-	int wt[] = { 2,3,4,5 };
-	int W = 5;
-	int n = sizeof(val) / sizeof(val[0]);
-	cout << knapSack(W, wt, val, n);
-	return 0;
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int count =0;
+        if((a+b+c)>=2 && (a+b+c)<=3){
+            count ++;
+        }
+        cout<<count <<endl;
+    }
 }
