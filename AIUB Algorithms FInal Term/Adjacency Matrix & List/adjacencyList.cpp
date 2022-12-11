05557397899d9bc101f48bfba4cd88c7ecfbf233
Badhon Nath Joy <8ad40n@gmail.com>
2022-12-12 01:47:05 +0600

    changed

--- AIUB Algorithms FInal Term/Adjacency Matrix & List/adjacencyList.cpp
+++ AIUB Algorithms FInal Term/Adjacency Matrix & List/adjacencyList.cpp
@@ -17,14 +17,57 @@ void add_edge(list<int> adj_list[], int u, int v) {
    adj_list[v].push_back(u);
 }
 int main() {
-   int v = 6;    
+   int v = 5;    
    list<int> adj_list[v];
-   add_edge(adj_list, 1, 2);
+   add_edge(adj_list, 0, 1);
+   add_edge(adj_list, 0, 2);
+   add_edge(adj_list, 0, 3);
+   add_edge(adj_list, 1, 4);
+   add_edge(adj_list, 4, 3);
+   add_edge(adj_list, 3, 1);
    add_edge(adj_list, 2, 3);
-   add_edge(adj_list, 2, 4);
-   add_edge(adj_list, 3, 5);
-   add_edge(adj_list, 4, 1);
-   add_edge(adj_list, 5, 4);
+
 
    displayAdjList(adj_list, v);
 }
+
+
+
+/*
+
+// Adjascency List representation in C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Add edge
+void addEdge(vector<int> adj[], int s, int d) {
+  adj[s].push_back(d);
+  adj[d].push_back(s);
+}
+
+// Print the graph
+void printGraph(vector<int> adj[], int V) {
+  for (int d = 0; d < V; ++d) {
+    cout << "\n Vertex "
+       << d << ":";
+    for (auto x : adj[d])
+      cout << "-> " << x;
+    printf("\n");
+  }
+}
+
+int main() {
+  int V = 5;
+
+  // Create a graph
+  vector<int> adj[V];
+
+  // Add edges
+  addEdge(adj, 0, 1);
+  addEdge(adj, 0, 2);
+  addEdge(adj, 0, 3);
+  addEdge(adj, 1, 2);
+  printGraph(adj, V);
+}
+*/
